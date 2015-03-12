d4de39a3246c6f427359816c98c5161e7a49f78f
haihui.wang <465495722@qq.com>
2015-03-12 20:02:52 +0800

    continue

---
+++ app/src/main/java/com/xiaobukuaipao/photocapture/PhotoCapture2Activity.java
@@ -0,0 +1,19 @@
+package com.xiaobukuaipao.photocapture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PhotoCapture2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_capture2);
+
+        if (null == savedInstanceState) {
+
+        }
+    }
+
+}
--- app/src/main/java/com/xiaobukuaipao/photocapture/view/CameraPreview.java
+++ app/src/main/java/com/xiaobukuaipao/photocapture/view/CameraPreview.java
@@ -2,10 +2,12 @@ package com.xiaobukuaipao.photocapture.view;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -13,6 +15,7 @@ import java.util.List;
  * Created by xiaobu1 on 15-3-11.
  */
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = CameraPreview.class.getSimpleName();
     // SurfaceHolder
     private SurfaceHolder mHolder;
     // Our Camera
@@ -28,14 +31,22 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     // View holding this camera
     private View mCameraView;
 
-
-
     public CameraPreview(Context context, Camera camera, View cameraView) {
         super(context);
         // Capture the context
         mCameraView = cameraView;
         mContext = context;
         setCamera(camera);
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // 保持屏幕始终开着
+        mHolder.setKeepScreenOn(true);
+
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     /**
@@ -52,23 +63,46 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
             mCamera.setParameters(parameters);
         }
-        
+
+        // 强制重绘,从measure开始
         requestLayout();
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
 
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        //
     }
 
     @Override
     public void	surfaceCreated(SurfaceHolder holder) {
-
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview : " + e.getMessage());
+        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        // Dispose of the camera preview.
+        if (mCamera != null) {
+            mCamera.stopPreview();
+        }
     }
 
 }
